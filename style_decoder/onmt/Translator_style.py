c1727f5387a129fd2396f1377ee832599eea46c0
wyu-du <duwanyu0706@163.com>
2020-04-08 13:07:40 -0400

    modify load

--- style_decoder/onmt/Translator_style.py
+++ style_decoder/onmt/Translator_style.py
@@ -9,8 +9,8 @@ class Translator(object):
         self.opt = opt
         self.tt = torch.cuda if opt.cuda else torch
 
-        checkpoint = torch.load(opt.decoder_model, map_location=lambda storage, loc: storage)
-        encoder_check = torch.load(opt.encoder_model, map_location=lambda storage, loc: storage)
+        checkpoint = torch.load(opt.decoder_model, map_location='cpu')
+        encoder_check = torch.load(opt.encoder_model, map_location='cpu')
 
         self.src_dict = encoder_check['dicts']['src']
         self.tgt_dict = checkpoint['dicts']['tgt']
